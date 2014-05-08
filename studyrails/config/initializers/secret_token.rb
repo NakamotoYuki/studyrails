f4e980be0ec8fe7a6af0d15d2ffbfc016ed0afe4
Nakamoto <nfmail9@gmail.com>
2014-05-08 11:34:57 +0900

    first

---
+++ studyrails/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Studyrails::Application.config.secret_key_base = 'aa6762f1c1f031f03ae86dce5af58694fcb924019d2840c5c379e56a2f6c1ee13050327a40eaca3a026bc6ff7088620977fa6d0e8f5e0e0db78732c5d1ff7c08'
